695d230871ab4e3634218a38940cafe6ddfd717f
SAO <SAO@DESKTOP>
2023-10-17 21:33:58 +0300

    Добавьте файлы проекта.

---
+++ 4.9 Домашнее задание/Program.cs
@@ -0,0 +1,78 @@
+﻿///////////////////////////////////////////////////////////////////
+// Задание 1. Приложение по определению чётного или нечётного числа
+///////////////////////////////////////////////////////////////////
+
+Console.WriteLine("Задание 1. Приложение по определению чётного или нечётного числа");
+Console.WriteLine("Введите число");
+int number;
+number = int.Parse(Console.ReadLine());
+
+string oddness;
+if (number % 2 == 0) oddness = "Четное";
+else oddness = "Нечетное";
+Console.WriteLine("Число {0}: {1}", number, oddness);
+
+////////////////////////////////////////////////////////
+// Задание 2. Программа подсчёта суммы карт в игре «21»
+////////////////////////////////////////////////////////
+
+Console.WriteLine("Задание 2.Программа подсчёта суммы карт в игре «21»");
+Console.WriteLine("Здравствуйте, введите число карт: ");
+number = int.Parse(Console.ReadLine());
+string input;
+int value;
+int sum = 0;
+
+Console.WriteLine("Вводите номера карт по одной");
+for (int i = 0; i < number; i++) 
+{ 
+    input = Console.ReadLine();
+    bool result = Int32.TryParse(input, out value);
+    switch (result)
+    {
+        case true:
+            sum += value; break;
+        case false:
+            sum += 10; break;
+    }
+}
+
+Console.WriteLine($"Сумма карт: {sum}");
+
+//////////////////////////////////////
+// Задание 3. Проверка простого числа
+//////////////////////////////////////
+
+Console.WriteLine("Задание 3. Проверка простого числа");
+Console.WriteLine("Введите число для проверки простоты: ");
+number = int.Parse(Console.ReadLine());
+bool odd = false;
+int counter = 1;
+while (!odd & counter < number)
+{
+    if (number % counter == 0) odd = true; break;
+    counter++;
+}
+
+switch (odd)
+{
+    case true: Console.WriteLine($"Число {number} - не является простым"); break; 
+    case false: Console.WriteLine($"Число {number} - является простым"); break;
+}
+
+/////////////////////////////////////////////////////
+// Задание 4. Наименьший элемент в последовательности
+/////////////////////////////////////////////////////
+
+Console.WriteLine("Задание 4. Наименьший элемент в последовательности");
+int min = int.MaxValue;
+Console.WriteLine("Введите длину последовательности: ");
+int length = Console.ReadLine();
+Console.WriteLine("Введите целые числа по одному");
+for(int i = 0; i<length; i++)
+{
+    number = int.Parse(Console.ReadLine());
+    if (number < min) min = number;
+}
+
+Console.WriteLine("");
